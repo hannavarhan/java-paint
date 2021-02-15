36627c22c9899e5eb3bf5e1dc492cfe614123f5a
hanna_varhan <hanna_varhan@bsu.by>
2021-02-15 20:56:29 +0300

    графический интерфейс на JavaFX

---
+++ src/sample/ShapeController.java
@@ -0,0 +1,74 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class ShapeController {
+
+    @FXML
+    private Pane drawPanel;
+
+    @FXML
+    private TextField userInfoArea;
+
+    @FXML
+    private Text parameterDescriptionField;
+
+    @FXML
+    private ColorPicker lineColorPicker;
+
+    @FXML
+    private ColorPicker fillColorPicker;
+
+    @FXML
+    private RadioButton lineBtn;
+
+    @FXML
+    private RadioButton rayBtn;
+
+    @FXML
+    private RadioButton triangleBtn;
+
+    @FXML
+    private RadioButton brokenLineBtn;
+
+    @FXML
+    private RadioButton segmentBtn;
+
+    @FXML
+    private RadioButton ellipseBtn;
+
+    @FXML
+    private RadioButton rhombusBtn;
+
+    @FXML
+    private RadioButton parallelogramBtn;
+
+    @FXML
+    private RadioButton circleBtn;
+
+    @FXML
+    private RadioButton rectangleBtn;
+
+    @FXML
+    private RadioButton regularPolygon;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
+
+
