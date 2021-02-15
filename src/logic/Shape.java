91e02e82bb8dc5bcb49846ec13ed9db848c7c361
vmodzolevskii <vmodzolevskii@gmail.com>
2021-02-16 01:12:40 +0300

    add simple classes for test

---
+++ src/logic/Shape.java
@@ -0,0 +1,31 @@
+package logic;
+
+import java.awt.*;
+
+public class Shape {
+    private Point theCenter;
+    private Color theLineColor;
+
+    public void draw() {
+    }
+
+    public void move() {
+
+    }
+
+    public Point getTheCenter() {
+        return theCenter;
+    }
+
+    public Color getTheLineColor() {
+        return theLineColor;
+    }
+
+    public void setTheCenter(Point theCenter) {
+        this.theCenter = theCenter;
+    }
+
+    public void setTheLineColor(Color theLineColor) {
+        this.theLineColor = theLineColor;
+    }
+}
